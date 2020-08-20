506f58ce41c54515e3de58d566bbc65af9e17b44
Ambre Vanneuville <ambre.vanneuville@gmail.com>
2020-08-20 20:16:26 +0200

    +

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-
-
+//barre de navigation responsive
 $(document).ready(function(){
   $('#icon').click(function(){
     $('ul').toggleClass('active');
   });
 
-
-
-
-
-
+//bouton scrolltotop de la page d'acceuil, pas encore paramètré CSS
   $('#btnScrollToTop').click(function(){
     window.scrollTo({
       top: 0,
