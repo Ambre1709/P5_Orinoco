153d16fa2882fab8b8a4974df28ee5635aaf6121
Ambre Vanneuville <ambre.vanneuville@gmail.com>
2020-10-11 19:13:34 +0200

    page accueil + images

--- main.js
+++ main.js
@@ -14,4 +14,34 @@ $(document).ready(function(){
   });
 });
 
+fetch ("http://localhost:3000/api/teddies")
+.then(response => response.json())
+.then(resp => {
+  console.log(resp)
+  for (let i = 0; i < resp.length; i++) {
+    afficherTeddy(resp[i])
+  }
+})
+//page acceuil
+function afficherTeddy(teddy){
+  let divIndex=document.createElement("div")
+  divIndex.className="image"
+  let aIndex=document.createElement("a")
+  aIndex.href="produit.html"
+  let imageIndex=document.createElement("img")
+  imageIndex.src=teddy.imageUrl
+  imageIndex.className="imageSize"
+  aIndex.appendChild(imageIndex)
+  divIndex.appendChild(aIndex)
+  let nameIndex=document.createElement("h3")
+  nameIndex.textContent=teddy.name
+   let priceIndex=document.createElement("h3")
+  priceIndex.textContent=teddy.price
+  divIndex.appendChild(nameIndex)
+  divIndex.appendChild(priceIndex)
+  document.getElementById("list_teddy").appendChild(divIndex)
+
+
+}
+
 
